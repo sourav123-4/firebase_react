57fef01a53b8bab45e7ad65f23698dda223be719
sourav12 <souravmahanty12@hmail.com>
2022-09-05 18:57:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    address: "",
+  });
+  let name, value;
+  const handleChange = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setUser({ ...user, [name]: value });
+  };
+  console.log("user", user);
+  const postData = async (e) => {
+    e.preventDefault();
+    const { name, phone, email, address } = user;
+    if (name && phone && email && address) {
+      const res = await fetch(
+        "https://formfirebase-da2ec-default-rtdb.firebaseio.com/reactform.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            phone,
+            email,
+            address,
+          }),
+        }
+      );
+      if (res) {
+        setUser({
+          name: "",
+          phone: "",
+          email: "",
+          address: "",
+        });
+        alert("data added succesfully");
+      }
+    } else {
+      alert("plz fill the data");
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form method="POST">
+        <input
+          type="name"
+          value={user.name}
+          onChange={handleChange}
+          name="name"
+          placeholder="Enter the name"
+          required
+        />
+        <input
+          type="phone"
+          value={user.phone}
+          onChange={handleChange}
+          name="phone"
+          placeholder="Enter the Phone no"
+          required
+        />
+        <input
+          type="email"
+          value={user.email}
+          onChange={handleChange}
+          name="email"
+          placeholder="Enter the Email address"
+          required
+        />
+        <input
+          type="address"
+          value={user.address}
+          onChange={handleChange}
+          name="address"
+          placeholder="Enter the Address"
+          required
+        />
+        <button onClick={postData}>Submit</button>
+      </form>
     </div>
   );
 }
